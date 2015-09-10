33aaea860d446e9dead71a2b6f25fc48c3a426c0
ddavisgraphics <ddavisgraphics@gmail.com>
2015-09-10 16:01:52 -0400

    modifying customer template variables to use db

--- client/client.js
+++ client/client.js
@@ -1,3 +1,21 @@
-Template.hello.welcomeMessage = function(){
-    return "Welcome to my app";
-};
+Template.customerTemplate.helpers({
+    // customers loop will loop through all customers
+    allCustomers:function(){
+        return Customers.find();
+    },
+    fullname:function(){
+        return this.firstname +" "+ this.lastname;
+    },
+    phone:function(){
+        return this.phone;
+    },
+    website:function(){
+        return this.website;
+    },
+    email: function(){
+        return this.email;
+    },
+    company:function(){
+        return this.companyName;
+    }
+});
